eebe62bb208802101bd2262a15674356ca4d7a00
nourhen youssfi <nourheneyoussfi03@gmail.com>
2025-04-01 23:21:06 +0100

    second try

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { ClientComponent } from './client/client.component';
 
 @Component({
   selector: 'app-root',
   imports: [
-/*AdminComponent,*/
-
+    RouterOutlet,
+AdminComponent,
+ClientComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import path from 'node:path';
 import { AdminComponent } from './admin/admin.component';
+import { ClientComponent } from './client/client.component';
 
 export const routes: Routes = [
     { path: 'admin', component: AdminComponent },
+    {path:'client' ,component:ClientComponent},
 ];
